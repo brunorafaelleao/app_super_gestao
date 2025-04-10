e37c001a98cb178c2cdd86b53990227a557a191b
Bruno Leão <bruno.melosh@gmail.com>
2025-04-10 13:55:20 -0400

    aprendendo sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Essa rota é para especificar o que será exibido na página inicial (raiz da aplicação)
 Route::get('/', function () {
-    return view('welcome');
+    return
+        'Olá Bruno!';
+    // view('welcome');
+});
+
+
+//Rota para a tela onde fala sobre nós
+Route::get('/sobre-nos', function () {
+    return
+        'Sobre nós';
+});
+
+//Rota para a tela de contato
+Route::get('/contato', function () {
+    return
+        'Contato';
 });
